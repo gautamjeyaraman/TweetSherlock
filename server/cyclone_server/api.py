028a02545beeccabb2d3a0babc87b9a9459251c4
gautamjeyaraman <gautam.jeyaraman@tataatsu.com>
2015-04-25 23:32:47 +0530

    Processing tweets and getting sentiment

--- server/cyclone_server/api.py
+++ server/cyclone_server/api.py
@@ -4,6 +4,9 @@ from twitter_client import TwitterClient
 from sentiment_client import SentimentClient
 import cyclone
 
+
+sentiment_classifier = SentimentClient()
+
 class APIBase(cyclone.web.RequestHandler):
 
     def get_config(self):
@@ -25,8 +28,33 @@ class TweetHandler(APIBase):
     def get(self):
         twitter_client = TwitterClient()
         query = self.get_argument("q")
+        print "Fetching tweets"
         data = twitter_client.getTweets(query)
+
+        print "Processing for sentiment"
         tweets = data['statuses']
+        response = []
+        for tweet in tweets:
+            if 'retweeted_status' in tweet.keys() and type(tweet['retweeted_status']) == type({}):
+                tweets.append(tweet['retweeted_status'])
+            row = { 'created_at': tweet['created_at'],
+                    'count': { 'favorite': tweet['favorite_count'],
+                               'retweet': tweet['retweet_count']
+                    },
+                    'text': tweet['text'],
+                    'hashtags': [x['text'] for x in tweet['entities']['hashtags']],
+                    'user': { 'description': tweet['user'].get('description', None),
+                              'favorite': tweet['user'].get('favorites_count', None),
+                              'followers': tweet['user'].get('followers_count', None),
+                              'location': tweet['user'].get('location', None),
+                              'name': tweet['user'].get('name', None),
+                              'image': tweet['user'].get('profile_image_url', None),
+                              'tweets': tweet['user'].get('statuses_count', None),
+                              'verified': tweet['user'].get('verified', None)
+                    },
+                    'sentiment': sentiment_classifier.getSentiment(tweet['text'])
+            }
+            response.append(row)
         
-        return self.write_json({'success': True, 'data': data})
+        return self.write_json({'success': True, 'data': response})
 
